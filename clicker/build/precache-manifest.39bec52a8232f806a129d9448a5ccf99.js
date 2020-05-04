e5da178f49dc8513628e8cd17ff2a5556c11261f
Kweku76 <daffran@gmail.com>
2020-05-04 13:14:49 +1000

    Add files via upload

---
+++ clicker/build/precache-manifest.39bec52a8232f806a129d9448a5ccf99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "865a9e59769121f1b0818e2daedf4668",
+    "url": "/react/clicker/index.html"
+  },
+  {
+    "revision": "7cc28b8dc5845760e829",
+    "url": "/react/clicker/static/css/main.d719787f.chunk.css"
+  },
+  {
+    "revision": "78f26a190e0781d81dee",
+    "url": "/react/clicker/static/js/2.8497bca0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/clicker/static/js/2.8497bca0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cc28b8dc5845760e829",
+    "url": "/react/clicker/static/js/main.09c31818.chunk.js"
+  },
+  {
+    "revision": "90280307858cbbebe695",
+    "url": "/react/clicker/static/js/runtime-main.3e88bfb9.js"
+  }
+]);
